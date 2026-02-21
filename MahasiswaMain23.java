d3331d125bdbb2fcf9786d6b62b4609a41a68545
ridoq <ridoq444@gmail.com>
2026-02-21 15:21:23 +0700

    Percobaan 3 - Soal 5

--- MahasiswaMain23.java
+++ MahasiswaMain23.java
@@ -1,18 +1,23 @@
 public class MahasiswaMain23 {
     public static void main(String[] args) {
         Mahasiswa23 mhs1 = new Mahasiswa23();
-        mhs1.nama = "Taufiqi Ridho Maulana";
-        mhs1.nim = "254107060058";
-        mhs1.kelas = "SIB 1F";
-        mhs1.ipk = 3.5;
+        mhs1.nama = "John Doe";
+        mhs1.nim = "25410000002";
+        mhs1.kelas = "SIB 1X";
+        mhs1.ipk = 3.99;
         
         mhs1.tampilkanInformasi();
         mhs1.ubahKelas("SIB 1Z");
-        mhs1.updateIpk(3.6);
+        mhs1.updateIpk(4.0);
         mhs1.tampilkanInformasi();
 
-        Mahasiswa23 mhs2 = new Mahasiswa23("Jane Doe", "254100000001", 3.99, "SIB 1F");
+        Mahasiswa23 mhs2 = new Mahasiswa23("Jane Doe", "254100000002", 3.99, "SIB 1Z");
         mhs2.updateIpk(4.00);
         mhs2.tampilkanInformasi();
+
+        Mahasiswa23 mhsRidok = new Mahasiswa23("Taufiqi Ridho Maulana", "254107060058", 3.5, "SIB 1Z");
+        mhsRidok.updateIpk(3.6);
+        mhsRidok.ubahKelas("SIB 1F");
+        mhsRidok.tampilkanInformasi();
     }
 }
